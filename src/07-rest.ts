0fee9ebbabda2e4e62c749ef33698e034ab8c740
Fabian Ardila <fabianra97@gmail.com>
2022-04-13 15:22:31 -0500

    rest parameters class

---
+++ src/07-rest.ts
@@ -0,0 +1,48 @@
+import { ROLES, User } from "./01-enum";
+
+const currentUser: User = {
+    username: 'fardila',
+    role: ROLES.CUSTOMER
+}
+
+export const checkAdminRole = () => {
+    if (currentUser.role === ROLES.ADMIN) {
+        return true;
+    }
+    return false;
+}
+
+const rta = checkAdminRole();
+console.log(rta);
+
+
+export const checkRole = (role1: ROLES, role2: ROLES) => {
+    if (currentUser.role === role1 || currentUser.role === role2) {
+        return true;
+    }
+    return false;
+}
+
+const rta2 = checkRole(ROLES.ADMIN, ROLES.SELLER);
+console.log(rta2);
+
+export const checkRoleV2 = (roles: string[]) => {
+    if (roles.includes(currentUser.role)) {
+        return true;
+    }
+    return false;
+}
+
+const rta3 = checkRoleV2([ROLES.ADMIN, ROLES.SELLER]);
+console.log(rta3);
+
+
+export const checkRoleV3 = (...roles: string[]) => {
+    if (roles.includes(currentUser.role)) {
+        return true;
+    }
+    return false;
+}
+
+const rta4 = checkRoleV3(ROLES.ADMIN, ROLES.SELLER, ROLES.CUSTOMER);
+console.log(rta4);
