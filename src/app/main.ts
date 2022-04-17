d2af08f0d5b48707fe2b3bebe887187529a88196
Fabian Ardila <fabianra97@gmail.com>
2022-04-17 12:37:35 -0500

    Implementing Interfaces class

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,11 +2,14 @@ import { addProduct } from "./products/product.service";
 
 addProduct({
     id: '1',
-    title: 'p1',
     createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'p1',
     stock: 90,
     category: {
         id: '12',
-        name: 'c1'
+        name: 'c1',
+        createdAt: new Date(),
+        updatedAt: new Date()
     }
 });
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+    data.createdAt = new Date();
     products.push(data);
 }
